49f64efddb7b78f78ed0c2a7e1bc64092dae546f
Chineze Nwosu <chineze.nwosu@andela.com>
2016-10-18 16:08:50 +0100

    failed: test user registration [#132673363]

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe "new" do
+  describe "GET #new" do
     it "renders a new page" do
       get :new
 
       expect(response).to render_template(:new)
+    end
+  end
+
+  describe "POST #create" do
+    it "creates a new user" do
+      post :create, params: {
+        email: "user@gmail.com",
+        password: "password",
+        password_confirmation: "password"
+      }
+
+      expect(controller).to respond_with(:ok)
+      expect(User.first).to be_present
     end
   end
 end
